0d88c4f6836522189cfffd246169198d71d16d1b
Salil Mandal <salilmandal908@gmail>
2026-05-14 14:14:08 +0530

    nextconfig update again

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ['@prisma/client', 'bcrypt'], 
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-  },
-    experimental: {
-    serverExternalPackages: ['@prisma/client'],
   },
 }
 export default nextConfig
